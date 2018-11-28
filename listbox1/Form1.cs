c6fe823a5d19b529137cb9285767c05cae1303dc
Tania <ladytatiana100@gmail.com>
2018-11-28 11:11:10 +0200

    first commit

---
+++ listbox1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace listbox1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Червоний");
+            listBox1.Items.Add("Помаранчовий");
+            listBox1.Items.Add("Жовтий");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Зелений");
+            listBox1.Items.Add("Голубий");
+            listBox1.Items.Add("Синій");
+            listBox1.Items.Add("Фіолетовий");
+
+        }
+    }
+}
